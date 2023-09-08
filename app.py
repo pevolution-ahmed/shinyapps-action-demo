cda030156975bf4627190e7eed70c7e2a7ee1e57
Ahmed Mamdouh <43514480+pevolution-ahmed@users.noreply.github.com>
2023-09-08 03:59:54 +0300

    add manifest.json file

--- app.py
+++ app.py
@@ -1,17 +1,37 @@
-from shiny import App, render, ui
+from shiny import App, render, ui,reactive
+import palmerpenguins
 
+
+penguins = palmerpenguins.load_penguins()
 app_ui = ui.page_fluid(
     ui.h2("Hello Shiny!",style="background-color:orange; color:white; "),
-    ui.input_slider("n", "N", 0, 100, 20),
-    ui.output_text_verbatim("txt"),
+    
+    ui.input_slider("n", "Input the Year:", 2007, 2009, 1000),
+    ui.output_text_verbatim("filtered_txt"),
+    ui.output_data_frame("grid"),
+
 )
 
 
 def server(input, output, session):
+    @reactive.Calc
+    def func1():
+        return penguins.loc[( penguins['species'].isin(['Chinstrap','Adelie']) ) & (penguins.year == int(input.n()))]
+    
     @output
     @render.text
-    def txt():
-        return f"n*2 is {input.n() * 2}"
+    def filtered_txt():
+        return func1()
+    
+    @output
+    @render.data_frame
+    def grid():
+        
+        return render.DataGrid(
+                        func1(),
+                        height="100%",
+                        width="100%",
+        )
 
 
 app = App(app_ui, server)
